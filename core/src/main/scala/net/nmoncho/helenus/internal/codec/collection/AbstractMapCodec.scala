0fd47be1c88eeb77fd46ff46da1374ce9cdedfdd
Gustavo De Micheli <gustavo.de.micheli@lunatech.com>
2022-10-06 18:03:18 +0200

    feature: Remove multiple map traversal/transformation, during encoding/decoding. Remove 'collections' package object

--- core/src/main/scala/net/nmoncho/helenus/internal/codec/collection/AbstractMapCodec.scala
+++ core/src/main/scala/net/nmoncho/helenus/internal/codec/collection/AbstractMapCodec.scala
@@ -29,6 +29,7 @@ import com.datastax.oss.driver.internal.core.`type`.codec.ParseUtils
 
 import java.nio.ByteBuffer
 import scala.collection.compat._
+import scala.collection.mutable
 
 abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
     keyInner: TypeCodec[K],
@@ -37,14 +38,10 @@ abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
 )(implicit factory: Factory[(K, V), M[K, V]])
     extends TypeCodec[M[K, V]] {
 
-  override def accepts(value: Any): Boolean = value match {
-    case m: Map[_, _] =>
-      m.forall { case (key, value) =>
-        keyInner.accepts(key) && valueInner.accepts(value)
-      }
-
-    case _ => false
-  }
+  private val openingChar: Char       = '{'
+  private val closingChar: Char       = '}'
+  private val entrySeparator: Char    = ','
+  private val keyValueSeparator: Char = ':'
 
   override val getCqlType: DataType =
     new DefaultMapType(keyInner.getCqlType, valueInner.getCqlType, frozen)
@@ -52,20 +49,34 @@ abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
   override def encode(value: M[K, V], protocolVersion: ProtocolVersion): ByteBuffer =
     if (value == null) null
     else {
-      // FIXME this seems pretty costly, we iterate the set several times!
-      val buffers = List.newBuilder[ByteBuffer]
-      for ((k, v) <- value) {
-        if (k == null) {
-          throw new IllegalArgumentException("Map keys cannot be null")
-        }
-        if (v == null) {
-          throw new IllegalArgumentException("Map values cannot be null")
-        }
-        buffers += keyInner.encode(k, protocolVersion)
-        buffers += valueInner.encode(v, protocolVersion)
+      val (buffers, size) = value.foldLeft((Vector.empty[ByteBuffer], 4)) {
+        case ((buffers, totalSize), (key, value)) =>
+          if (key == null) {
+            throw new IllegalArgumentException("Map keys cannot be null")
+          }
+          if (value == null) {
+            throw new IllegalArgumentException("Map values cannot be null")
+          }
+
+          val encodedKey   = keyInner.encode(key, protocolVersion)
+          val encodedValue = valueInner.encode(value, protocolVersion)
+          val size         = (4 + encodedKey.remaining()) + (4 + encodedValue.remaining())
+
+          (
+            buffers :+ encodedKey :+ encodedValue,
+            totalSize + size
+          )
       }
 
-      pack(buffers.result().toArray, value.size, protocolVersion)
+      val result = ByteBuffer.allocate(size)
+      result.putInt(value.size)
+      for (element <- buffers) {
+        result.putInt(element.remaining())
+        result.put(element)
+      }
+      result.flip()
+
+      result
     }
 
   override def decode(bytes: ByteBuffer, protocolVersion: ProtocolVersion): M[K, V] = {
@@ -76,24 +87,54 @@ abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
       val input = bytes.duplicate()
       val size  = input.getInt()
       for (_ <- 0 until size) {
-        val k = keyInner.decode(readValue(input, protocolVersion), protocolVersion)
-        val v = valueInner.decode(readValue(input, protocolVersion), protocolVersion)
-        builder += k -> v
+        // Allow null elements on the decode path, because Cassandra might return such collections
+        // for some computed values in the future -- e.g. SELECT ttl(some_collection)
+
+        // Decode Key
+        val keySize = input.getInt()
+        val key =
+          if (keySize < 0) null.asInstanceOf[K]
+          else {
+            val copy = input.duplicate()
+            copy.limit(copy.position() + keySize)
+            input.position(input.position() + keySize)
+            keyInner.decode(copy, protocolVersion)
+          }
+
+        // Decode Value
+        val valueSize = input.getInt()
+        val value =
+          if (valueSize < 0) null.asInstanceOf[V]
+          else {
+            val copy = input.duplicate()
+            copy.limit(copy.position() + valueSize)
+            input.position(input.position() + valueSize)
+
+            valueInner.decode(copy, protocolVersion)
+          }
+
+        builder += key -> value
       }
 
       builder.result()
     }
   }
 
-  override def format(value: M[K, V]): String =
-    if (value == null) {
+  override def format(map: M[K, V]): String =
+    if (map == null) {
       "NULL"
     } else {
-      value
-        .map { case (key, value) =>
-          s"${keyInner.format(key)}:${valueInner.format(value)}" // Using a SB would yield better performance
-        }
-        .mkString("{", ",", "}")
+      val sb   = new mutable.StringBuilder().append(openingChar)
+      var tail = false
+      for ((key, value) <- map) {
+        if (tail) sb.append(entrySeparator)
+        else tail = true
+
+        sb.append(keyInner.format(key))
+          .append(keyValueSeparator)
+          .append(valueInner.format(value))
+      }
+      sb.append(closingChar).toString()
     }
 
   override def parse(value: String): M[K, V] =
@@ -102,13 +143,20 @@ abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
     } else {
       val builder = factory.newBuilder
       var idx     = ParseUtils.skipSpaces(value, 0)
-      if (value.charAt(idx) != '{') {
+
+      if (idx >= value.length) {
         throw new IllegalArgumentException(
-          s"Cannot parse map value from '$value', at character $idx expecting '{' but got '${value.charAt(idx)}''"
+          s"Malformed map value '$value', missing opening '$openingChar', but got EOF"
+        )
+      } else if (value.charAt(idx) != openingChar) {
+        throw new IllegalArgumentException(
+          s"Cannot parse map value from '$value', at character $idx expecting '$openingChar' but got '${value
+              .charAt(idx)}''"
         )
       }
+
       idx = ParseUtils.skipSpaces(value, idx + 1)
-      if (value.charAt(idx) == '}') {
+      if (value.charAt(idx) == closingChar) {
         builder.result()
       } else {
         while (idx < value.length) {
@@ -116,10 +164,14 @@ abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
           val n = ParseUtils.skipCQLValue(value, idx)
           val k = keyInner.parse(value.substring(idx, n))
           idx = ParseUtils.skipSpaces(value, n)
-          if (idx >= value.length || value.charAt(idx) != ':') {
+          if (idx >= value.length) {
             throw new IllegalArgumentException(
-              s"Cannot parse map value from '$value', at character $idx expecting ':' but got '${value
-                  .charAt(idx)}''"
+              s"Cannot parse map value from '$value', at character $idx expecting '$keyValueSeparator' but got EOF"
+            )
+          } else if (value.charAt(idx) != keyValueSeparator) {
+            throw new IllegalArgumentException(
+              s"Cannot parse map value from '$value', at character $idx expecting '$keyValueSeparator' but got '${value
+                  .charAt(idx)}'"
             )
           }
 
@@ -132,20 +184,30 @@ abstract class AbstractMapCodec[K, V, M[K, V] <: Map[K, V]](
           idx = ParseUtils.skipSpaces(value, nv)
           if (idx >= value.length) {
             throw new IllegalArgumentException(
-              s"Malformed map value '$value', missing closing '}'"
+              s"Malformed map value '$value', missing closing '$closingChar', but got EOF"
             )
-          } else if (value.charAt(idx) == '}') {
+          } else if (value.charAt(idx) == closingChar) {
             return builder.result()
-          } else if (value.charAt(idx) != ',') {
+          } else if (value.charAt(idx) != entrySeparator) {
             throw new IllegalArgumentException(
-              s"Cannot parse map value from '$value', at character $idx expecting ',' but got '${value
+              s"Cannot parse map value from '$value', at character $idx expecting '$entrySeparator' but got '${value
                   .charAt(idx)}''"
             )
           }
           idx = ParseUtils.skipSpaces(value, idx + 1)
         }
-        throw new IllegalArgumentException(s"Malformed map value '$value', missing closing '}'")
+        throw new IllegalArgumentException(
+          s"Malformed map value '$value', missing closing '$closingChar'"
+        )
       }
     }
 
+  override def accepts(value: Any): Boolean = value match {
+    case m: Map[_, _] =>
+      m.headOption.exists { case (key, value) =>
+        keyInner.accepts(key) && valueInner.accepts(value)
+      }
+
+    case _ => false
+  }
 }
