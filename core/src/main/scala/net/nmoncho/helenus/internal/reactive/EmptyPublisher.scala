965b210fb2784a567a4292834dbd525d2845bd8c
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2024-01-13 22:51:46 +0100

    chore: Add some documentation on reactive classes

--- core/src/main/scala/net/nmoncho/helenus/internal/reactive/EmptyPublisher.scala
+++ core/src/main/scala/net/nmoncho/helenus/internal/reactive/EmptyPublisher.scala
@@ -24,6 +24,10 @@ package net.nmoncho.helenus.internal.reactive
 import org.reactivestreams.Publisher
 import org.reactivestreams.Subscriber
 
+/** [[Publisher]] implementation that doesn't publish any element
+  *
+  * @tparam A published element type
+  */
 class EmptyPublisher[A] extends Publisher[A] {
   override def subscribe(s: Subscriber[_ >: A]): Unit = {
     require(s != null, "Subscriber must not be null")
--- core/src/main/scala/net/nmoncho/helenus/internal/reactive/TakeOperator.scala
+++ core/src/main/scala/net/nmoncho/helenus/internal/reactive/TakeOperator.scala
@@ -25,7 +25,13 @@ import org.reactivestreams.Publisher
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
-class TakeOperator[A](pub: Publisher[A], take: Int) {
+/** Provides a [[Publisher]] that will publish at most [[amount]] elements
+  *
+  * @param pub original published where elements are coming from
+  * @param amount at most amount of elements to publish
+  * @tparam A published element type
+  */
+class TakeOperator[A](pub: Publisher[A], amount: Int) {
 
   def publisher: Publisher[A] = new Publisher[A] {
     override def subscribe(s: Subscriber[_ >: A]): Unit = {
@@ -43,12 +49,12 @@ class TakeOperator[A](pub: Publisher[A], take: Int) {
       subscriber.onSubscribe(s)
 
     override def onNext(t: A): Unit = {
-      if (count < take) {
+      if (count < amount) {
         subscriber.onNext(t)
       }
 
       count += 1
-      if (count >= take) {
+      if (count >= amount) {
         subscriber.onComplete()
       }
     }
