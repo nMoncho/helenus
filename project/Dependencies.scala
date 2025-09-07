ae14f88a88d8fe649a672fbac387d9ca0f5a3b25
Scala Steward <scala_steward@virtuslab.com>
2025-09-07 18:14:43 +0000

    Update scalacheck to 1.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
 
     // Test Dependencies
     val mockito       = "5.16.0"
-    val scalaCheck    = "1.18.1"
+    val scalaCheck    = "1.19.0"
     val scalaTest     = "3.2.19"
     val scalaTestPlus = "3.2.18.0"
     val logback       = "1.5.17"
