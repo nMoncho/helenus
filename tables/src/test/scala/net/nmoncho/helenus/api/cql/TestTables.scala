0afc7ccd38193522aa143f0e5df96a7a80588164
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2026-07-11 15:45:46 +0200

    feat(tables): add `ColumnNames` for materializing column names from declarations

--- tables/src/test/scala/net/nmoncho/helenus/api/cql/TestTables.scala
+++ tables/src/test/scala/net/nmoncho/helenus/api/cql/TestTables.scala
@@ -23,12 +23,12 @@ case class User(
 object UsersTable extends Table[User]("my_keyspace", "users") {
   import net.nmoncho.helenus._
 
-  val id: Column[UUID]       = column[UUID]("id")
-  val username: Column[String] = column[String]("username")
-  val age: Column[Int]      = column[Int]("age")
-  val email: Column[String]    = column[String]("email")
-  val tags: Column[Set[String]]     = column[Set[String]]("tags")
-  val metadata: Column[Map[String,String]] = column[Map[String, String]]("metadata")
+  val id: Column[UUID]                      = column[UUID]("id")
+  val username: Column[String]              = column[String]("username")
+  val age: Column[Int]                      = column[Int]("age")
+  val email: Column[String]                 = column[String]("email")
+  val tags: Column[Set[String]]             = column[Set[String]]("tags")
+  val metadata: Column[Map[String, String]] = column[Map[String, String]]("metadata")
 
   type PK = id.Tag :: HNil
   type CK = username.Tag :: HNil
