7cfd0ab6c65289fb192d7a337db82b158abff233
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2026-07-11 12:14:28 +0200

    feat(tables): enforce all fields/columns registered for a given case class

--- tables/src/test/scala/net/nmoncho/helenus/api/cql/TestTables.scala
+++ tables/src/test/scala/net/nmoncho/helenus/api/cql/TestTables.scala
@@ -8,6 +8,9 @@ package net.nmoncho.helenus.api.cql
 
 import java.util.UUID
 
+import shapeless.::
+import shapeless.HNil
+
 case class User(
     id: UUID,
     username: String,
@@ -17,4 +20,20 @@ case class User(
     metadata: Map[String, String]
 )
 
-object UsersTable extends Table[User]("my_keyspace", "users") {}
+object UsersTable extends Table[User]("my_keyspace", "users") {
+  import net.nmoncho.helenus._
+
+  val id: Column[UUID]       = column[UUID]("id")
+  val username: Column[String] = column[String]("username")
+  val age: Column[Int]      = column[Int]("age")
+  val email: Column[String]    = column[String]("email")
+  val tags: Column[Set[String]]     = column[Set[String]]("tags")
+  val metadata: Column[Map[String,String]] = column[Map[String, String]]("metadata")
+
+  type PK = id.Tag :: HNil
+  type CK = username.Tag :: HNil
+
+  override protected val columns: Table.AllColumns = registerAllColumns(
+    id :: username :: age :: email :: tags :: metadata :: HNil
+  )
+}
