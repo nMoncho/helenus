162b1a8f00342dba2c9004adf813fc7b916cf744
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2026-07-13 20:06:12 +0200

    chore(tables): add basic `Predicate` to include in `SELECT FROM` DSL

---
+++ tables/src/main/scala/net/nmoncho/helenus/api/cql/dml/where/package.scala
@@ -0,0 +1,11 @@
+package net.nmoncho.helenus.api.cql.dml
+
+package object where {
+
+  /**
+   * Marker for values accepted by `where`: single predicates and conjunctions
+   * built with `and`.
+   */
+  trait WhereClause
+
+}
