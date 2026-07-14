4d95b03b6f1c3011294ec5eb1f62ae172b4e185c
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2026-07-14 06:30:10 +0200

    chore(tables): introduce query type parameters that will be used to define if a query requires filtering or not

--- tables/src/main/scala/net/nmoncho/helenus/api/cql/dml/where/package.scala
+++ tables/src/main/scala/net/nmoncho/helenus/api/cql/dml/where/package.scala
@@ -13,4 +13,10 @@ package object where {
     */
   trait WhereClause
 
+  /** Phantom marker recorded when a predicate can never be part of a valid
+    * primary-key restriction (`!==`, `contains`). Its presence in the
+    * constraint state makes the ungated `execute` unavailable, leaving only
+    * `allowFiltering.execute`.
+    */
+  sealed trait RequiresFiltering
 }
