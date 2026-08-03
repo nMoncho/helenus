e0406cc17ae32f83ba93d0b0c88363b8c0579996
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2026-08-03 15:45:56 +0200

    test(core): add missing tests on `Adapter`

--- core/src/test/scala/net/nmoncho/helenus/api/cql/AdapterSpec.scala
+++ core/src/test/scala/net/nmoncho/helenus/api/cql/AdapterSpec.scala
@@ -6,6 +6,7 @@
 
 package net.nmoncho.helenus.api.cql
 
+import net.nmoncho.helenus.api.cql.AdapterSpec.Person
 import net.nmoncho.helenus.models.Address
 import net.nmoncho.helenus.models.Hotel
 import net.nmoncho.helenus.utils.HotelsTestData
@@ -21,19 +22,39 @@ class AdapterSpec extends AnyWordSpec with Matchers {
     "adapt a case class to a tuple" in {
       val adapter = Adapter[Hotel]
 
-      adapter(Hotels.h1) should equal(
-        "h1",
-        "The James Rotterdam",
-        "+31 10 710 9000",
-        Address(
-          "Wijnhaven 107",
-          "Rotterdam",
-          "South Holland",
-          "3011 WN",
-          "Netherlands"
-        ),
-        Set(rotterdamErasmusBridge.name, rotterdamZoo.name)
+      // Binding to an explicit tuple type pins the derived output arity and
+      // element types, not just the values.
+      val adapted: (String, String, String, Address, Set[String]) = adapter(Hotels.h1)
+
+      adapted shouldBe (
+        (
+          "h1",
+          "The James Rotterdam",
+          "+31 10 710 9000",
+          Address(
+            "Wijnhaven 107",
+            "Rotterdam",
+            "South Holland",
+            "3011 WN",
+            "Netherlands"
+          ),
+          Set(rotterdamErasmusBridge.name, rotterdamZoo.name)
+        )
       )
+    }
+
+    "adapt a small case class to a tuple" in {
+      val adapter = Adapter[Person]
+
+      val adapted: (String, Int) = adapter(Person("Alice", 30))
+
+      adapted shouldBe (("Alice", 30))
+    }
+
+    "build an adapter without computed columns equal to the no-op adapter" in {
+      val built = Adapter.builder[Hotel].build
+
+      built(Hotels.h1) shouldBe Adapter[Hotel].apply(Hotels.h1)
     }
 
     "adapt a case class with a computed column" in {
@@ -42,20 +63,58 @@ class AdapterSpec extends AnyWordSpec with Matchers {
         .withComputedColumn(_.name.charAt(0))
         .build
 
-      adapter(Hotels.h1) should equal(
-        "h1",
-        "The James Rotterdam",
-        "+31 10 710 9000",
-        Address(
-          "Wijnhaven 107",
-          "Rotterdam",
-          "South Holland",
-          "3011 WN",
-          "Netherlands"
-        ),
-        Set(rotterdamErasmusBridge.name, rotterdamZoo.name),
-        'T' // computed column
+      val adapted: (String, String, String, Address, Set[String], Char) = adapter(Hotels.h1)
+
+      adapted shouldBe (
+        (
+          "h1",
+          "The James Rotterdam",
+          "+31 10 710 9000",
+          Hotels.h1.address,
+          Set(rotterdamErasmusBridge.name, rotterdamZoo.name),
+          'T' // computed column
+        )
+      )
+    }
+
+    "adapt a case class with a computed column derived from several fields" in {
+      val adapter = Adapter
+        .builder[Person]
+        .withComputedColumn(p => s"${p.name}:${p.age}")
+        .build
+
+      val adapted: (String, Int, String) = adapter(Person("Alice", 30))
+
+      adapted shouldBe (("Alice", 30, "Alice:30"))
+    }
+
+    "append multiple computed columns in the order they are declared" in {
+      val adapter = Adapter
+        .builder[Hotel]
+        .withComputedColumn(_.name.charAt(0))
+        .withComputedColumn(_.pois.size)
+        .withComputedColumn(_.phone.startsWith("+"))
+        .build
+
+      val adapted: (String, String, String, Address, Set[String], Char, Int, Boolean) =
+        adapter(Hotels.h1)
+
+      adapted shouldBe (
+        (
+          "h1",
+          "The James Rotterdam",
+          "+31 10 710 9000",
+          Hotels.h1.address,
+          Hotels.h1.pois,
+          'T', // first computed column
+          2, // second computed column: number of POIs
+          true // third computed column: phone is international
+        )
       )
     }
   }
+}
+
+object AdapterSpec {
+  final case class Person(name: String, age: Int)
 }
